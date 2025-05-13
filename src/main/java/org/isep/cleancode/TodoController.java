1439439e501716ef009e9ac4c93332c5a809cd1c
L34k1 <152170478+L34k1@users.noreply.github.com>
2025-05-13 19:39:24 +0200

    Update TodoController.java

--- src/main/java/org/isep/cleancode/TodoController.java
+++ src/main/java/org/isep/cleancode/TodoController.java
@@ -1,32 +1,45 @@
-package org.isep.cleancode;
+package org.isep.cleancode.presentation;
 
 import com.google.gson.Gson;
+import org.isep.cleancode.Todo;
+import org.isep.cleancode.application.TodoManager;
+import org.isep.cleancode.persistence.inmemory.TodoInMemoryRepository;
 import spark.Request;
 import spark.Response;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class TodoController {
-
-    // this Todo class should be completed to achieve Step 1
-
     private static final Gson gson = new Gson();
-    private final List<Todo> todos = new ArrayList<>();
+    private final TodoManager todoManager;
+
+    public TodoController() {
+        // Use in-memory repository for Step 1
+        this.todoManager = new TodoManager(new TodoInMemoryRepository());
+    }
 
     public Object getAllTodos(Request req, Response res) {
         res.type("application/json");
-
+        List<Todo> todos = todoManager.getAllTodos();
         return gson.toJson(todos);
-    };
+    }
 
     public Object createTodo(Request req, Response res) {
-        Todo newTodo = gson.fromJson(req.body(), Todo.class);
+        try {
+            Todo newTodo = gson.fromJson(req.body(), Todo.class);
 
-        todos.add(newTodo);
-        res.status(201);
-        res.type("application/json");
+            if (newTodo == null || !newTodo.isValid()) {
+                res.status(400);
+                return "Invalid Todo: Name must be 1-64 characters and due date (if provided) must be in yyyy-MM-dd format and in the future";
+            }
 
-        return gson.toJson(newTodo);
-    };
+            todoManager.addTodo(newTodo);
+            res.status(201);
+            res.type("application/json");
+            return gson.toJson(newTodo);
+        } catch (Exception e) {
+            res.status(500);
+            return "Error creating todo: " + e.getMessage();
+        }
+    }
 }
