09240c5cc1b1edccb68807e3e9b479c56398ebc8
L34k1 <152170478+L34k1@users.noreply.github.com>
2025-05-13 19:39:09 +0200

    Update Todo.java

--- src/main/java/org/isep/cleancode/Todo.java
+++ src/main/java/org/isep/cleancode/Todo.java
@@ -1,20 +1,51 @@
 package org.isep.cleancode;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Todo {
-
-    // this Todo class should be completed to achieve Step 1
-
     private String name;
+    private String dueDate;
 
     public Todo(String name) {
         this.name = name;
+    }
+
+    public Todo(String name, String dueDate) {
+        this.name = name;
+        this.dueDate = dueDate;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public boolean nameIsValid() {
+        return name != null && !name.trim().isEmpty() && name.length() <= 64;
+    }
+
+    public boolean dueDateIsValid() {
+        if (dueDate == null) {
+            return true; // dueDate is optional
+        }
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        dateFormat.setLenient(false);
+
+        try {
+            Date dueDateObject = dateFormat.parse(dueDate);
+            return dueDateObject.after(new Date());
+        } catch (ParseException e) {
+            return false;
+        }
+    }
+
+    public boolean isValid() {
+        return nameIsValid() && dueDateIsValid();
     }
 }
